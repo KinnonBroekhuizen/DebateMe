b3b2eb7d9d59d0625b8c00113e18bda69f8dc928
Kiday <kiday.personal@protonmail.com>
2026-05-06 09:50:01 +1200

    On branch Kiday---Landing-Page-&-Opponent-Select Changes to be committed: modified:   app/layout.tsx modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
-import { supabase } from '@/lib/supabase'
-
 export default async function Home() {
-  const {data,error} = await supabase
-  .from('TestingTable')
-  .select('*')
-
-  console.log(data, error)
   return (
     <div>
       <main>
         <h1>Debate Me</h1>
-        {data?.map((row) => (
-          <div key={row.id}>
-            <h2>{row.name}</h2>
-            <p>{row.content}</p>
-          </div>
-        ))}
       </main>
     </div>
   );
