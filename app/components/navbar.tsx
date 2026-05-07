28d2c46509c2d748af4e01fc389ac8f169439bd8
Kiday <kiday.personal@protonmail.com>
2026-05-07 16:53:37 +1200

    Changed the icons' sizes on the landing page

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <header className="px-8 py-5 bg-[var(--primary)] ">
+    <header className="px-8 py-5 bg-[var(--primary)]">
       <Link href="/" className="text-2xl font-bold text-black ">
         Debate me.
       </Link>
